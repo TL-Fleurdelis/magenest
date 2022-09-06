c752972722af658fe0edf45b1e025ba9f4d566c3
TL-Fleurdelis <thanhlong6320@gmail.com>
2022-09-06 10:07:41 +0700

    create repo exam magenest

---
+++ customaddons/exam_1/__manifest__.py
@@ -0,0 +1,32 @@
+{
+    'name': "Advanced Sale Management",
+    'summary': """
+    Advance Sales
+""",
+    'summary_vi_VN': """
+Bán hàng nâng cao  
+""",
+
+    'description': """
+
+
+""",
+    'author': "TL-Fleurdelis",
+    'website': "https://github.com/TL-Fleurdelis",
+    'category': 'Uncategorized',
+    'version': '0.1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'sale_management', 'l10n_de','website_sale'],
+    # always loaded
+    'data': [
+        'security/new_customer_security.xml',
+        'security/ir.model.access.csv',
+        'views/new_sale_order_views.xml',
+        'views/new_customer_views.xml',
+        'views/new_menu_views.xml',
+        'wizard/customer_wizard_views.xml',
+        'views/templates.xml',
+    ],
+
+}
---
+++ customaddons/exam_2/wizard/warranty_wizard.py
@@ -0,0 +1,37 @@
+from odoo import models, fields,api
+
+
+class WarrantyWizard(models.Model):
+    _name = 'warranty.wizard'
+    _description = 'Warranty Wizard'
+    product_id = fields.Many2many('product.template', string='Product')
+    wiz_date_from = fields.Date()
+    wiz_date_to = fields.Date()
+    wiz_product_warranty = fields.Text()
+    @api.onchange('wiz_date_from', 'wiz_date_to')
+    def _onchange_code_warranty(self):
+        if self.wiz_date_from and self.wiz_date_to:
+            str_from, str_to = str(self.wiz_date_from), str(self.wiz_date_to)
+
+            # str_day_from, str_month_from, str_year_from = \
+            #     str(self.wiz_date_from.day), str(self.wiz_date_from.month), str(self.wiz_date_from.year)[2:]
+            #
+            # str_day_to, str_month_to, str_year_to =\
+            #     str(self.wiz_date_to.day), str(self.wiz_date_to.month), str(self.wiz_date_to.year)[2:]
+
+            # code = 'PWD'+'/'+ str_from +'/'+ str_to
+
+            label_day_from = str_from[8:10] + str_from[5:7] + str_from[2:4]
+            label_day_to = str_to[8:10] + str_to[5:7] + str_to[2:4]
+            code = 'PWD' + '/' + label_day_from + '/' + label_day_to
+
+            self.wiz_product_warranty = code
+        else:
+            self.wiz_product_warranty = ''
+    def set_warranty_for_product(self):
+        self.product_id.write({
+
+            'date_from': self.wiz_date_from,
+            'date_to': self.wiz_date_to,
+            'product_warranty': self.wiz_product_warranty,
+        })
